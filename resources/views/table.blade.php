4c1c15efdefc825cca05088464969614718cb917
sophidaa <sophida.c@kkumail.com>
2023-05-15 14:17:56 +0700

    Add modal to table

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -108,7 +108,7 @@
                                             <td> </td>
                                             <td> </td>
                                             <td> </td>
-                                            <td> </td>
+                                            <td> <button class="btn btn-success btn-user btn-block" id="myBtn">รายละเอียด</button> </td>
                                         </tr>
                                         <tr>
                                             <td> </td>
@@ -147,7 +147,7 @@
     <!-- End of Content Wrapper -->
 
 </div>
-<!-- End of Page Wrapper -->
+
 
 <!-- Scroll to Top Button-->
 <a class="scroll-to-top rounded" href="#page-top">
@@ -183,4 +183,52 @@
   <script src="{{ asset('js/jquery.min.js') }}"></script>
     </section>
 
+    <div id="myModal" class="modal">
+ <!-- Modal content -->
+ <div class="modal-dialog modal-lg" role="document">
+					<div class="modal-content">
+						<div class="modal-header">
+							<h5 class="modal-title" id="demoModalLabel">รายละเอียด อปท.</h5>
+								<button type="button" class="close" data-dismiss="modal" aria- 
+                                label="Close">
+									<span aria-hidden="true">&times;</span>
+								</button>
+						</div>
+						<div class="modal-body">
+								Welcome, Websolutionstuff !!
+						</div>
+					</div>
+				</div>
+			</div>
+
+</div>
+
+<script>
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[1];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+if (event.target == modal) {
+modal.style.display = "none";
+}
+}
+</script>
+</script>
 </x-app-layout>
